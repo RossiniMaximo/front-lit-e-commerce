63fb8ac6a4014cb04a9548e2e66c65e84cc21c60
Maximo <maximorossini2016@gmail.com>
2022-06-27 12:42:02 -0300

    using lazy loading to improve webapp perfomance

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "../components/layout/Layout";
 import { Home } from "../pages/home/Home";
 import { Search } from "../pages/search/Search";
 import { Item } from "../pages/item/Item";
-import { SignUp } from "../pages/signUp/SignUp";
 import { Code } from "../pages/code/Code";
-import { Cart } from "../pages/cart";
-import { Password } from "../pages/password";
+
+const differPassword = lazy(() => import("../pages/password"));
+const differSignUp = lazy(() => import("../pages/signUp/SignUp"));
+const differCart = lazy(() => import("../pages/cart"));
 
 export function Router() {
   return (
@@ -16,9 +17,9 @@ export function Router() {
         <Route index element={<Home />}></Route>
         <Route path="search/:query" element={<Search />}></Route>
         <Route path="item/:id" element={<Item />}></Route>
-        <Route path="sign-up" element={<SignUp />}></Route>
-        <Route path="cart" element={<Cart />}></Route>
-        <Route path="password" element={<Password />}></Route>
+        <Route path="sign-up" element={<differSignUp />}></Route>
+        <Route path="cart" element={<differCart />}></Route>
+        <Route path="password" element={<differPassword />}></Route>
       </Route>
       <Route path="/code" element={<Code />}></Route>
     </Routes>
